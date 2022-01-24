d0c8c293eb582d04abee31172509a07dc393ba23
Кирилл Ерохин <93985111+KirillErohin@users.noreply.github.com>
2022-01-24 19:07:09 +0300

    Add files via upload

---
+++ homework2/javascript.js
@@ -0,0 +1,31 @@
+// Возраста
+let userAge = 20;
+userAge = 19;
+console.log(userAge);
+
+// Имена
+let userName = 'Kirill'
+userName = 'Tima'
+console.log(userName);
+
+// Фамилия
+
+let userSurname = 'Erohin'
+userSurname = 'Buganov'
+console.log(userSurname)
+
+
+// Придумайте названия для
+
+let newUser;
+let listShop;
+let userOnline;
+let moneyWork;
+let cursorCoordinates;
+
+console.log('Hello, World!');
+
+let message;
+message = 'Hello, World!';
+
+alert(message)
